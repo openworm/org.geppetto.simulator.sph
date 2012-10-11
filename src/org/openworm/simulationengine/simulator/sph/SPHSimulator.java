1ed5d9524e996cd610f6f013ae5097c428077b83
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2012-10-11 15:28:10 +0100

    Initial commit for the SPH Simulator skeleton.

---
+++ src/org/openworm/simulationengine/simulator/sph/SPHSimulator.java
@@ -0,0 +1,49 @@
+package org.openworm.simulationengine.simulator.sph;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openworm.simulationengine.core.model.IModel;
+import org.openworm.simulationengine.core.simulation.ITimeConfiguration;
+import org.openworm.simulationengine.core.simulator.AParallelSimulator;
+import org.openworm.simulationengine.core.solver.ISolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SPHSimulator extends AParallelSimulator {
+
+	private static Log logger = LogFactory.getLog(SPHSimulator.class);
+	
+	private ITimeConfiguration _timeConfiguration;
+	
+	@Autowired
+	@Qualifier("sphSolver")
+	private ISolver sphSolver;
+	
+	@Override
+	public void startSimulatorCycle() {
+		logger.debug("SPH Simulator cycle started");
+		clearQueue();		
+	}
+
+	@Override
+	public void simulate(IModel model, ITimeConfiguration timeConfiguration) {
+		enqueue(model, sphSolver);
+		_timeConfiguration=timeConfiguration;
+		
+	}
+
+	@Override
+	public void endSimulatorCycle() {
+		logger.debug("cycle finished");
+		List<List<IModel>> results=sphSolver.solve(getQueue(sphSolver), _timeConfiguration);
+		for(List<IModel> models:results)
+		{
+			getListener().resultReady(models);
+		}
+	}
+
+}
