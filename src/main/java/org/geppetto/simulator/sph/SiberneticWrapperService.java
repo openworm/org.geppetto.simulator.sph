93730c1b4993c263d15390815396faf7f6559a1a
Sergey Khayrulin <s.khayrulin@gmail.com>
2015-07-27 18:47:19 +0600

    Add SiberneticWrapperService class

---
+++ src/main/java/org/geppetto/simulator/sph/SiberneticWrapperService.java
@@ -0,0 +1,33 @@
+package org.geppetto.simulator.sph;
+
+import org.geppetto.core.simulator.AExternalProcessSimulator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SiberneticWrapperService extends AExternalProcessSimulator {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void registerGeppettoService() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSimulatorPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
